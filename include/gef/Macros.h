e5d6ae4e65ecd05f9023dccdf4af24b012031e8e
cielixer <cielixer@gmail.com>
2026-02-12 21:08:41 +0900

    make GEF_MODULE to take function pointer of module body

--- include/gef/Macros.h
+++ include/gef/Macros.h
@@ -12,8 +12,8 @@
 #define GEF__BINDING_SENTINEL {nullptr, static_cast<gef_role_t>(0), nullptr}
 
 #define GEF_MODULE(name_, version_, execute_fn_, ...)                                              \
-    static constexpr gef_binding_t gef__bindings[] = {                                             \
-        __VA_OPT__(__VA_ARGS__, ) GEF__BINDING_SENTINEL};                                          \
+    static constexpr gef_binding_t gef__bindings[] = {__VA_OPT__(__VA_ARGS__, )                    \
+                                                          GEF__BINDING_SENTINEL};                  \
     static constexpr size_t gef__num_bindings =                                                    \
         (sizeof(gef__bindings) / sizeof(gef__bindings[0])) - 1u;                                   \
     static const gef_metadata_t gef__metadata = {                                                  \
@@ -22,7 +22,11 @@
         .bindings     = gef__bindings,                                                             \
         .num_bindings = gef__num_bindings,                                                         \
     };                                                                                             \
-    extern "C" const gef_metadata_t* gef_get_metadata(void) { return &gef__metadata; }             \
-    extern "C" void gef_execute(gef::Context& ctx) { execute_fn_(ctx); }
+    extern "C" const gef_metadata_t* gef_get_metadata(void) {                                      \
+        return &gef__metadata;                                                                     \
+    }                                                                                              \
+    extern "C" void gef_execute(gef::Context& ctx) {                                               \
+        execute_fn_(ctx);                                                                          \
+    }
 
 #endif
